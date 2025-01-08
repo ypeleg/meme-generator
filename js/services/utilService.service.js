f64f1de5a3763476988a5a9d1bb26ffa4b01cede
Yam Peleg <ypeleg2@gmail.com>
2025-01-08 13:58:33 +0200

    submission 14:00

---
+++ js/services/utilService.service.js
@@ -0,0 +1,18 @@
+function makeId(length = 6) {
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var txt = ''
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return txt
+}
+
+function saveToStorage(key, val) {
+  localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+  var val = localStorage.getItem(key)
+  return JSON.parse(val)
+}
